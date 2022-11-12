60a70def592eb1c8da62f425c487de33ec7a83bd
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-11-12 17:48:46 +0330

    fix channel bug

--- chat/consumers.py
+++ chat/consumers.py
@@ -23,7 +23,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.target_user = await self.get_target_user()
         self.target_chat = await self.get_chat_room()
-        self.room_group_name = f"chat_{self.room_name}"
+        self.room_group_name = f"chat_{str(self.target_chat.id)}"
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
