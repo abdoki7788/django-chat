74fe0a81920a4f169c9a7ecd6fb723fa140d6e54
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-11-25 15:21:29 +0330

    add slug field to chat model

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from random import randint
 
 User = get_user_model()
 
@@ -8,7 +9,13 @@ app_name = 'chat'
 class Chat(models.Model):
     starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='started_chats')
     participant = models.ForeignKey(User, on_delete=models.CASCADE, related_name='participated_chats')
+    slug = models.SlugField(null=True, blank=True)
     start_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = randint(1000000000000000, 9999999999999999)
+        return super(Chat, self).save(*args, **kwargs)
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sended_messages')
