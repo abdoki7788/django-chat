3c7fab497b7d3d5c787e7131fd6bd3c1b5859c5d
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-11-26 19:37:03 +0330

    set LOGOUT_REDIRECT_URL as index page

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'chat:index'
+LOGOUT_REDIRECT_URL = 'chat:index'
